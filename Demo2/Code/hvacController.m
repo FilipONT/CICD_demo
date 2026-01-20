d73ec0242daeea8d5de3bd3ba1cf7944086c0cbe
FilipBabiec <FilipBabiec@3AE_FBab_LAP1>
2026-01-20 12:05:29 +0100

    Added Demo2

---
+++ Demo2/Code/hvacController.m
@@ -0,0 +1,36 @@
+function [mode, powerCommand] = hvacController( ...
+    currentTemp, setpoint, occupancy)
+%HVACCONTROLLER Simple HVAC control logic
+%
+% Inputs:
+%   currentTemp - Current ambient temperature [°C]
+%   setpoint    - Desired temperature [°C]
+%   occupancy   - Boolean flag (true if occupied)
+%
+% Outputs:
+%   mode         - 'HEAT', 'COOL', or 'OFF'
+%   powerCommand - Normalized power command [0..1]
+
+% Default outputs
+mode = "OFF";
+powerCommand = 0.0;
+
+% Deadband to avoid oscillations
+deadband = 0.5;
+
+% If building is unoccupied, reduce energy usage
+if ~occupancy
+    return;
+end
+
+temperatureError = setpoint - currentTemp;
+
+if temperatureError > deadband
+    mode = "HEAT";
+    powerCommand = min(temperatureError / 10.0, 1.0);
+
+elseif temperatureError < -deadband
+    mode = "COOL";
+    powerCommand = min(abs(temperatureError) / 10.0, 1.0);
+end
+end
